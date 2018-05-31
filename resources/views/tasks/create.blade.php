f0cdd0b96eac2ac3cdd86292453865535b53bdf7
jiro.kirameki <ec2-user@ip-172-31-1-118.us-east-2.compute.internal>
2018-05-31 06:54:18 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,21 @@
 
     <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        <div class="row">
+        <div class="col-xs-6">
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
-
+        </div>
+        
+        <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
-
+        </div>
     
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
 
